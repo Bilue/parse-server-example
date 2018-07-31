4729cabb69957a360923cf90460624326b49a9a9
Craig Edwards <craig@blackdogfoundry.com>
2018-07-31 13:52:03 +1000

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (!databaseUri) {
 }
 
 var S3Adapter = require('parse-server').S3Adapter;
-console.log(S3Adapter);
+console.log(S3Adapter());
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
@@ -22,7 +22,7 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-  "filesAdapter": "@parse/s3-files-adapter"
+  filesAdapter: S3Adapter
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
